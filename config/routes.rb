5bad5e4cee737585726ee0dc718d51fefb3416fc
KoheiKusuyama <144124225+kohei-kusuyama@users.noreply.github.com>
2023-10-20 09:56:42 +0000

    [start]Meshitero

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+  
+  get 'homes/about' => "homes#about" ,as:  'about'
+  # get 'homes/top'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "homes#top"
+
+end
